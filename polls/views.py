a180c3925cf3c9d62b36f77ba9bff7d9176f601d
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-11-14 17:38:33 +0900

    add tests

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question
 
@@ -10,9 +11,19 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
+    # def get_queryset(self):
+    #     """
+    #     Return the last five published questions (not including those set to be
+    #     published in the future).
+    #     """
+    #     return Question.objects.filter(
+    #         pub_date__lte=timezone.now()
+    #     ).order_by('-pub_date')[:5]
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class DetailView(generic.DetailView):
     model = Question
