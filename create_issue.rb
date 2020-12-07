46b4f03cc59afa0d750375841c2d1f6658221b90
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-12-07 11:08:10 +0900

    add user name

--- create_issue.rb
+++ create_issue.rb
@@ -9,4 +9,4 @@ end
 require 'octokit'
 client = Octokit::Client.new(:access_token => ENV['GH_API_ACCESS_TOKEN'])
 
-client.create_issue('4geru/django-tutorial', 'mew', 'ok', {labels: 'hogehoge', milestone: 1})
+client.create_issue('4geru/django-tutorial', 'mew', 'ok', {labels: 'hogehoge', milestone: 1, assignees: [ENV['USER']]})
